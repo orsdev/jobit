7202b319f8ae100da07d2ed6811db2d755a4a876
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-05-25 19:37:03 +0100

    Updates

---
+++ precache-manifest.bd60319b1b3a9aa10a5b04032881845e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57dd4f50aa7dfd1d92bc2bb8497eea6e",
+    "url": "/jobit/index.html"
+  },
+  {
+    "revision": "d8ef6098ac4469f5d2b3",
+    "url": "/jobit/static/css/main.b6adf93c.chunk.css"
+  },
+  {
+    "revision": "aec17e1dd157c874cbfc",
+    "url": "/jobit/static/js/2.e07c5b51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jobit/static/js/2.e07c5b51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ef6098ac4469f5d2b3",
+    "url": "/jobit/static/js/main.4df4921b.chunk.js"
+  },
+  {
+    "revision": "79b3a834ef30ee89b035",
+    "url": "/jobit/static/js/runtime-main.cf0afc35.js"
+  },
+  {
+    "revision": "4e84872991fc11b4a22f0cd14c1c9942",
+    "url": "/jobit/static/media/hero-bg1.4e848729.png"
+  },
+  {
+    "revision": "f54e2439519253649cdac0c742252188",
+    "url": "/jobit/static/media/hero-bg2.f54e2439.png"
+  }
+]);
