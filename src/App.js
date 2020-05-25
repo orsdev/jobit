9439745dc9b64922cf27327df8e559540612540f
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-05-25 04:40:11 +0100

    hero.js component added to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/header';
+import Hero from './components/hero';
 import './assets/css/style.css';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/hero.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import HEROBG from '../assets/img/hero-bg1.png';
+
+const hero = () => (
+  <div className="hero">
+    <div className="hero__body">
+      <h1>FIND YOUR DREAM JOB</h1>
+      <p>
+        Aliquam vestibulum cursus felis.
+        In iaculis iaculis sapien ac condimentum.
+        Vestibulum congue posuere lacus,
+        id tincidunt nisi porta sit amet.
+      </p>
+      <form
+        action=""
+        id="search"
+        onSubmit={(e) => { e.preventDefault() }}>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            name="title"
+            placeholder="Job Title or Company Name"
+            required />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            name="location"
+            placeholder="City or State"
+            required />
+        </div>
+        <div className="form-search-btn">
+          <button
+            type="submit"
+            className="search-btn"> Search </button>
+        </div>
+      </form>
+    </div>
+    <div className="hero__img">
+      <img src={HEROBG} alt="HERO BACKGROUND" />
+    </div>
+  </div>
+);
+
+export default hero;
