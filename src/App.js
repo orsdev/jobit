02f21a774d27ede151fedbcd07d7af5cca431f4f
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-05-25 08:17:57 +0100

    jobs component added to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/header';
 import Hero from './components/hero';
 import './assets/css/style.css';
+import Jobs from './components/Jobs';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Hero />
+      <Jobs />
     </div>
   );
 }
---
+++ src/components/Jobs.js
@@ -0,0 +1,172 @@
+import React from 'react';
+
+const jobs = () => {
+  return (
+    <div className="jobs">
+      <h1> Recent Jobs </h1>
+      <div className="jobs__board">
+        <div className="jobs__board-item">
+          <div className="jobs__board-item-left">
+            <h3>Senior Manager</h3>
+            <a href="#">company.com</a>
+            <ul>
+              <li>
+                <i className="icofont-money-bag"></i>
+               $20k - $25k
+               </li>
+              <li>
+                <i className="icofont-location-pin"></i>
+             Location 210-27 Quadra, Market Street, Victoria Canada
+             </li>
+            </ul>
+          </div>
+          <div className="jobs__board-item-right">
+            <ul>
+              <li className="apply">
+                <a href="#">Apply</a>
+              </li>
+              <li>
+                <a className="job-type" href="#">Full Time</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="jobs__board-item">
+          <div className="jobs__board-item-left">
+            <h3>Software Engineer</h3>
+            <a href="#">software.com</a>
+            <ul>
+              <li>
+                <i className="icofont-money-bag"></i>
+               $120k - $250k
+               </li>
+              <li>
+                <i className="icofont-location-pin"></i>
+             Location 210-27 Quadra, Market Street, Vi Canada
+             </li>
+            </ul>
+          </div>
+          <div className="jobs__board-item-right">
+            <ul>
+              <li className="apply">
+                <a href="#">Apply</a>
+              </li>
+              <li>
+                <a className="job-type" href="#">Remote</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="jobs__board-item">
+          <div className="jobs__board-item-left">
+            <h3>Data Engineer</h3>
+            <a href="#">company.com</a>
+            <ul>
+              <li>
+                <i className="icofont-money-bag"></i>
+               $90k - $125k
+               </li>
+              <li>
+                <i className="icofont-location-pin"></i>
+             Location 210-27 Quadra, Market Street, Victoria Canada
+             </li>
+            </ul>
+          </div>
+          <div className="jobs__board-item-right">
+            <ul>
+              <li className="apply">
+                <a href="#">Apply</a>
+              </li>
+              <li>
+                <a className="job-type" href="#">Part Time</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="jobs__board-item">
+          <div className="jobs__board-item-left">
+            <h3>Product Designer</h3>
+            <a href="#">company.com</a>
+            <ul>
+              <li>
+                <i className="icofont-money-bag"></i>
+               $80k - $95k
+               </li>
+              <li>
+                <i className="icofont-location-pin"></i>
+             Location 210-27 Quadra, Market Street, Victoria Canada
+             </li>
+            </ul>
+          </div>
+          <div className="jobs__board-item-right">
+            <ul>
+              <li className="apply">
+                <a href="#">Apply</a>
+              </li>
+              <li>
+                <a className="job-type" href="#">Full Time</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="jobs__board-item">
+          <div className="jobs__board-item-left">
+            <h3>Customer Support Specialist</h3>
+            <a href="#">company.com</a>
+            <ul>
+              <li>
+                <i className="icofont-money-bag"></i>
+               $60k - $75k
+               </li>
+              <li>
+                <i className="icofont-location-pin"></i>
+             Location 210-27 Quadra, Market Street, Victoria Canada
+             </li>
+            </ul>
+          </div>
+          <div className="jobs__board-item-right">
+            <ul>
+              <li className="apply">
+                <a href="#">Apply</a>
+              </li>
+              <li>
+                <a className="job-type" href="#">Freelance</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="jobs__board-item">
+          <div className="jobs__board-item-left">
+            <h3>IT Manager</h3>
+            <a href="#">company.com</a>
+            <ul>
+              <li>
+                <i className="icofont-money-bag"></i>
+               $20k - $25k
+               </li>
+              <li>
+                <i className="icofont-location-pin"></i>
+             Location 210-27 Quadra, Market Street, Victoria Canada
+             </li>
+            </ul>
+          </div>
+          <div className="jobs__board-item-right">
+            <ul>
+              <li className="apply">
+                <a href="#">Apply</a>
+              </li>
+              <li>
+                <a className="job-type" href="#">Contract</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="jobs__footer">
+        <button type="button"> Load More </button>
+      </div>
+    </div>
+  )
+};
+
+export default jobs;
